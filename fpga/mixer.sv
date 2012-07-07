271b1d6f34f77ca45e95b072074cefbdfa303a21
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2012-07-07 19:45:22 -0400

    Live demo of ADAT input used this code.

--- fpga/mixer.sv
+++ fpga/mixer.sv
@@ -6,12 +6,15 @@ module mixer(
 	output wire[7:0] LED
 );
 
+wire data_request;
+
 bit signed [23:0] audio_out [0:7];
 adat_out adat_out_0(
         .clk(adat_bitclock),
         .rst(0), .timecode(0), .smux(0),
         .audio_bus(audio_out),
-        .adat_bitstream(adat_out)
+        .adat_bitstream(adat_out),
+		  .data_request(data_request)
         );
 
 wire signed [23:0] audio_in [0:7];
@@ -23,5 +26,20 @@ adat_in adat_in_0(
         .data_valid(adat_data_valid),
         .audio_bus(audio_in)
         );
+
+always @(posedge data_request) begin
+	integer i;
+	for (i=0; i<8; i++) audio_out[i] = audio_in[i];
+end
+
+bit [23:0] max_level = 0;
+wire [23:0] abs_val;
+assign abs_val = audio_in[0][23] ? -audio_in[0] : audio_in[0];
+
+always_ff @(posedge data_request) begin
+	if (abs_val > max_level) max_level <= abs_val;
+end
+
+assign LED = abs_val[23:16];
 
 endmodule
