70084b282c5588dc27328c1ac22a576b79edaf49
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2012-08-10 13:41:45 -0400

    Parameterize the saturation module.

--- fpga/saturate.sv
+++ fpga/saturate.sv
@@ -1,24 +1,28 @@
-module saturate (
-    input wire [35:0] in,
+module saturate #(
+    parameter IN_WIDTH = 36,
+    parameter OUT_WIDTH = 24,
+    parameter HEADROOM = 6
+    )(
+    input wire [IN_WIDTH-1:0] in,
     output logic overflow,
-    output logic [23:0] out);
+    output logic [OUT_WIDTH-1:0] out);
     
-    // in (36-24 bit example):
+    // in (36->24 bit example):
     // 35 ... 30 29 ... 6 5 ... 0
     //  ^------^  ^-----^  ^----^
     //  headroom    num     precision
     always_comb begin
-        if ({6{in[35]}} != in[34:29]) begin
+        if ({HEADROOM{in[IN_WIDTH-1]}} != in[IN_WIDTH-2:IN_WIDTH-HEADROOM-1]) begin
             // uh oh, clipped!
             overflow <= 1'b1;
-            if (in[35] == 0) begin
-                out <= {1'b0, {23{1'b1}}}; // largest positive number
+            if (in[IN_WIDTH-1] == 0) begin
+                out <= {1'b0, {(OUT_WIDTH-1){1'b1}}}; // largest positive number
             end else begin
-                out <= {1'b1, {23{1'b0}}}; // largest negative number
+                out <= {1'b1, {(OUT_WIDTH-1){1'b0}}}; // largest negative number
             end
         end else begin
             overflow <= 1'b0;
-            out <= in[29:6];
+            out <= in[IN_WIDTH-HEADROOM-1:IN_WIDTH-HEADROOM-OUT_WIDTH];
         end
     end
     
