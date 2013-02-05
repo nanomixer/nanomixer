24834de2859a0e90daa68a03fb506f2a5f51a49c
Martin Segado <msegado@mit.edu>
2013-02-05 16:06:23 -0500

    Added the saturator! (and io writeback, too)

--- fpga_v2/dsp_core.sv
+++ fpga_v2/dsp_core.sv
@@ -59,10 +59,10 @@ logic signed [ACCUM_WIDTH-1:0] M, next_M, // data registers & next-state variabl
                                
 logic [LFSR_WIDTH-1:0] lfsr, next_lfsr;   // LFSR register and next-state variable
 
-logic signed [SAMPLE_WIDTH-1:0] saturated_A, // saturator output
-                                mult_in1;    // first multiplier input
+logic signed [SAMPLE_WIDTH-1:0] mult_in1, // first multiplier input
+                                saturator_out;
 
-logic signed [PARAM_WIDTH-1:0] mult_in2;     // second multiplier input
+logic signed [PARAM_WIDTH-1:0] mult_in2;  // second multiplier input
                                 
 instr_t decode_instr,
         read_instr,
@@ -70,6 +70,11 @@ instr_t decode_instr,
         ex2_instr, 
         writeback_instr;
 
+logic [ACCUM_SINT_BITS - IO_SINT_BITS-1 : 0] io_truncated_MSBs;
+logic [ACCUM_SINT_BITS - SAMPLE_SINT_BITS-1 : 0] sample_truncated_MSBs;
+logic io_sign_bit, io_is_saturated,
+      sample_sign_bit, sample_is_saturated;
+      
         
 /***** COMBINATORIAL LOGIC: *****/
 
@@ -109,17 +114,39 @@ always_comb begin
          next_lfsr = (lfsr >> 1);
       
       // Saturation & Writeback:
-      ring_bus_out = A; // inter-dsp communication output
+      io_truncated_MSBs = A[ACCUM_WIDTH-1 : ACCUM_FRAC_BITS + IO_SINT_BITS];
+      io_sign_bit       = A[ACCUM_FRAC_BITS + IO_SINT_BITS-1];      
+      io_is_saturated   = signed'(io_truncated_MSBs) != signed'(io_sign_bit);
+
+      sample_truncated_MSBs = A[ACCUM_WIDTH-1 : ACCUM_FRAC_BITS + SAMPLE_SINT_BITS];
+      sample_sign_bit       = A[ACCUM_FRAC_BITS + SAMPLE_SINT_BITS-1];
+      sample_is_saturated   = signed'(sample_truncated_MSBs) != signed'(sample_sign_bit);
       
-      saturated_A = A[PARAM_FRAC_BITS + SAMPLE_WIDTH-1 -: SAMPLE_WIDTH];  
-            // TODO: currently *truncates*; add saturation logic
+      case (writeback_instr.opcode)
+         OUT     : if (io_is_saturated)
+                         saturator_out = signed'({A[ACCUM_WIDTH-1], {(IO_WIDTH-1){~A[ACCUM_WIDTH-1]}} });
+                   else  saturator_out = A[ACCUM_FRAC_BITS + SAMPLE_SINT_BITS-1 -: SAMPLE_WIDTH];
+
+         default : if (sample_is_saturated)
+                        saturator_out = signed'({A[ACCUM_WIDTH-1], {(SAMPLE_WIDTH-1){~A[ACCUM_WIDTH-1]}} });
+                   else saturator_out = A[ACCUM_FRAC_BITS + SAMPLE_SINT_BITS-1 -: SAMPLE_WIDTH];
+      endcase
       
+      io_mem.wr_data = saturator_out[SAMPLE_FRAC_BITS + IO_SINT_BITS-1 -: IO_WIDTH];
+      io_mem.wr_addr = writeback_instr.param_addr;
+      case (writeback_instr.opcode)
+         OUT     : io_mem.wr_en = 1'b1;
+         default : io_mem.wr_en = 1'b0;
+      endcase
+      
+      sample_mem.wr_data = saturator_out;
       sample_mem.wr_addr = writeback_instr.sample_addr;
-      
-      case (writeback_instr.opcode)  // handle accumulator inputs
+      case (writeback_instr.opcode)
          IN, STORE : sample_mem.wr_en = 1'b1;
          default   : sample_mem.wr_en = 1'b0;
       endcase
+
+      ring_bus_out = A; // inter-dsp communication output
 end
  
 assign test_out = lfsr; // TODO: Remove once testing is complete
