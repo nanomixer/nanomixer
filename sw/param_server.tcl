6a3fe988452aab3e23c970b2ccc44b3fc554d3ed
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2013-03-16 19:39:17 -0400

    Massive change to the parameter server so that it also dumps metering.
    This probably doesn't work yet.

--- sw/param_server.tcl
+++ sw/param_server.tcl
@@ -1,6 +1,3 @@
-global usbblaster_name
-global test_device
-global instance_indices
 # List all available programming hardwares, and select the USBBlaster.
 # (Note: this example assumes only one USBBlaster connected.)
 # Programming Hardwares:
@@ -29,36 +26,37 @@ foreach instance [get_editable_mem_instances -hardware_name $usbblaster_name -de
     dict set instance_indices $name $index
 }
 puts "Instance $instance_indices";
+set metering_instance [dict get $instance_indices "MMEM"]
 
-proc Start_Server {port} {
-	set s [socket -server ConnAccept $port]
-	puts "Started Socket Server on port - $port"
-	vwait forever
-}
+# This server is continuously editing memory.
+begin_memory_edit -hardware_name $usbblaster_name -device_name $test_device
 
-	
-proc ConnAccept {sock addr port} {
-    global conn
 
-    # Record the client's information
-
-    puts "Accept $sock from $addr port $port"
-    set conn(addr,$sock) [list $addr $port]
-
+proc AcceptParamServerConnection {sock addr port} {
     # Ensure that each "puts" by the server
     # results in a network transmission
     fconfigure $sock -buffering line
 
-    # Set up a callback for when the client sends data
-    fileevent $sock readable [list IncomingData $sock]
-
-    OpenPort
+    # Set up a callback following when the client sends data
+    fileevent $sock readable [list IncomingParamData $sock]
 }
 
+proc AcceptMeteringConnection { sock addr port} {
+    # Ensure that each "puts" by the server
+    # results in a network transmission
+    fconfigure $sock -buffering line
 
-proc IncomingData {sock} {
-    global conn
+    fileevent $sock writable [list WriteMeteringData $sock]
+}
 
+proc WriteMeteringData {sock} {
+    global metering_instance
+    set metering_data [read_content_from_memory -content_in_hex -instance_index metering_instance -start_address 0 -word_count 8]
+    puts -nonewline $sock $metering_data
+    flush $sock
+}
+
+proc IncomingParamData {sock} {
     # Check end of file or abnormal connection drop,
     # then write the data to the vJTAG
 
@@ -69,34 +67,30 @@ proc IncomingData {sock} {
 	set content [read $sock $length]}]} {
 
 	close $sock
-	puts "Close $conn(addr,$sock)"
-	unset conn(addr,$sock)
 	ClosePort
     } else {
 	setMemContent $mem_name $addr $content
-	puts -nonewline $sock "1"
+    # Read metering memory contents.
+    puts -nonewline $sock "1"
 	flush $sock
     }
 }
 
-
-proc OpenPort {} {
-    global usbblaster_name
-    global test_device
-    begin_memory_edit -hardware_name $usbblaster_name -device_name $test_device
-}
-
-proc ClosePort {} {
-    end_memory_edit
-}
-
 proc setMemContent {mem_name addr content} {
-	 global instance_indices
+	global instance_indices
     set instance_idx [dict get $instance_indices $mem_name]
     set content_words [expr [string length $content] / 9]
     puts "Setting $content_words at $mem_name:$addr to $content"
     write_content_to_memory -instance_index $instance_idx -start_address $addr -content $content -word_count $content_words -content_in_hex
 }
 
-#Start thet Server at Port 2540
-Start_Server 2540
+
+set paramServerPort 2540
+set meterServerPort 2541
+socket -server AcceptParamServerConnection $paramServerPort
+socket -server AcceptMeteringConnection $meterServerPort
+
+puts "Started Socket Server on port $paramServerPort"
+vwait forever
+
+end_memory_edit
