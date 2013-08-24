62956c7e20144fed96f5286e774331cc0c8d9bc8
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2013-08-24 17:20:54 -0400

    Add memory I/O ports.

--- spi/spi_slave.sv
+++ spi/spi_slave.sv
@@ -1,5 +1,5 @@
 module spi_slave #(
-	PARAM_WIDTH = 40,
+	PARAM_WIDTH = 36,
     ADDR_WIDTH = 8
 ) (
 	input wire clk,
@@ -8,19 +8,19 @@ module spi_slave #(
 	input wire spi_SCLK, // spi clock
 	input wire spi_SSEL, // spi slave select
 	input wire spi_MOSI, // data in
-	output logic spi_MISO // data out
+	output logic spi_MISO, // data out
     
-//    // Memory read port
-//    output logic[ADDR_WIDTH-1:0] rd_addr;
-//    input logic[PARAM_WIDTH-1:0] rd_data;
-//
-//    // Memory write port
-//    output logic[ADDR_WIDTH-1:0] wr_addr;
-//    output logic[PARAM_WIDTH-1:0] wr_data;
-//    output logic wr_enable;
-);
+    // Memory read port
+    output logic[ADDR_WIDTH-1:0] rd_addr,
+    input logic[PARAM_WIDTH-1:0] rd_data,
 
-localparam COUNT_WIDTH = $clog2(PARAM_WIDTH+1);
+    // Memory write port
+    output logic[ADDR_WIDTH-1:0] wr_addr,
+    output logic[PARAM_WIDTH-1:0] wr_data,
+    output logic wr_enable
+);
+localparam PACKET_SIZE = PARAM_WIDTH + 4;
+localparam COUNT_WIDTH = $clog2(PACKET_SIZE + 1);
 
 // States:
 // START: start of transmission, before the first sclk edge
@@ -44,10 +44,10 @@ always_comb begin : proc_clkedges
 end
 
 logic [COUNT_WIDTH-1:0] bitsRemaining, bitsRemaining_next;
-logic [PARAM_WIDTH-1:0] inputReg, inputReg_next, outputReg, outputReg_next, toOutput;
+logic [PACKET_SIZE-1:0] inputReg, inputReg_next, outputReg, outputReg_next, toOutput;
 logic loadOutput, dataReady, dataReady_next;
 always_comb begin : proc_serdes
-	spi_MISO = outputReg[PARAM_WIDTH-1];
+	spi_MISO = outputReg[PACKET_SIZE-1];
 
 	// defaults (no latches!)
     dataReady_next = '0;
@@ -55,15 +55,15 @@ always_comb begin : proc_serdes
 	inputReg_next = inputReg;
 	outputReg_next = outputReg;
 
-	if (spi_SSEL) begin
+	if (ssel) begin
 		// reset.
-		bitsRemaining_next = PARAM_WIDTH-1;
+		bitsRemaining_next = PACKET_SIZE-1;
 		inputReg_next = '0;
 		outputReg_next = '0;
         dataReady_next = '0;
 	end else if (sclk_posedge) begin
 		// shift out.
-		inputReg_next = {inputReg[PARAM_WIDTH-2:0], mosi};
+		inputReg_next = {inputReg[PACKET_SIZE-2:0], mosi};
 		outputReg_next = outputReg << 1;
         dataReady_next = '0;
 	end else if (sclk_negedge) begin
@@ -71,7 +71,7 @@ always_comb begin : proc_serdes
 		if (bitsRemaining) begin
 			bitsRemaining_next = bitsRemaining_next - 1;
 		end else begin
-			bitsRemaining_next = PARAM_WIDTH-1;
+			bitsRemaining_next = PACKET_SIZE-1;
             dataReady_next = '1;
 		end
 	end else if (loadOutput) begin
@@ -89,15 +89,29 @@ end
 
 
 // Memory interface
-//logic loadOutput_next;
-//logic [PARAM_WIDTH-1:0] toOutput_next;
+logic loadOutput_next;
+logic [PACKET_SIZE-1:0] toOutput_next;
+logic [PARAM_WIDTH-1:0] rd_addr_next;
 always_comb begin
-    loadOutput = '0;
-    toOutput = '0;
-    if (dataReady) begin
-        loadOutput = '1;
-        toOutput = inputReg;
+    loadOutput_next = '0;
+    toOutput_next = toOutput;
+    rd_addr_next = rd_addr;
+    
+    if (ssel) begin
+        // Reset
+        rd_addr_next = 'b0;
+    end else if (dataReady) begin
+        rd_addr_next = rd_addr + 'b1;
+        loadOutput_next = '1;
+        toOutput_next = {2'b0, rd_data};
     end
+end
+
+initial rd_addr = 0;
+always_ff @(posedge clk) begin : proc_memif
+    rd_addr <= rd_addr_next;
+    loadOutput <= loadOutput_next;
+    toOutput <= toOutput_next;
 end
 
 endmodule
