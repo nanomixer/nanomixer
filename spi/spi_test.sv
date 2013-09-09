a22620c73722ac7e064199cb800ae6cde5ce01d0
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2013-09-08 23:42:37 -0400

    [SPI] Untested updates to the tests.

--- spi/spi_test.sv
+++ spi/spi_test.sv
@@ -1,6 +1,6 @@
 module spi_test #(
     parameter real CLK_FREQ = 100.0e6,
-    parameter int WORD_WIDTH = 8);
+    parameter int PACKET_WIDTH = 8);
 
 localparam time CLK_PERIOD = (1.0e9/CLK_FREQ)*1ns;
 localparam time SPI_PERIOD = 10*CLK_PERIOD;
@@ -15,41 +15,36 @@ logic spi_SSEL = 0; // spi slave select
 logic spi_MOSI = 0; // data in
 logic spi_MISO; // data out
 
-spi_slave u1 (.clk, .spi_SCLK, .spi_SSEL, .spi_MOSI, .spi_MISO);
+logic [PACKET_WIDTH-1:0] txData;
+logic load;
+logic [PACKET_WIDTH-1:0] rxShiftReg;
+logic dataReady;
 
-// spies
-wire [WORD_WIDTH-1:0] inputReg = u1.inputReg, outputReg = u1.outputReg;
-wire sclk_posedge = u1.sclk_posedge, sclk_negedge=u1.sclk_negedge;
-
-logic [WORD_WIDTH-1:0] miso_buf;
+spi_serdes #(.PACKET_WIDTH(PACKET_WIDTH)) u1 
+    (.clk, .spi_SCLK, .spi_SSEL, .spi_MOSI, .spi_MISO,
+     .txData, .load, .rxShiftReg, .dataReady);
 
 // clock is active high and the first sampling happens on the first falling edge.
 
-task spi_xfer(logic [WORD_WIDTH-1:0] x);
+task spi_xfer(logic [PACKET_WIDTH-1:0] x);
 begin
     int bitIdx;
-    logic [WORD_WIDTH-1:0] mosi_buf;
-    miso_buf = 'bx;
     
     // Let's try to mimic how McSPI is configured: master data output begins a half-clock
     // before the first posedge.
     spi_SCLK = 0;
     #(SPI_PERIOD);
     $display("spi_xfer %x", x);
-    mosi_buf = x;
-    spi_MOSI = mosi_buf[WORD_WIDTH-1];
-    $display("spi_MOSI = %x", mosi_buf);
     
-    // now start ticking. Sample and advance both at negedge.
-    for (bitIdx=0; bitIdx<WORD_WIDTH; bitIdx++) begin
+    // now start ticking. Advance at negedge.
+    for (bitIdx=PACKET_WIDTH-1; bitIdx>=0; bitIdx--) begin
+        assert (dataReady == '0) else $error("Premature dataReady");
+        spi_MOSI = x[bitIdx];
         #(SPI_PERIOD/2) spi_SCLK = 1;
-        miso_buf <<= 1;
-        mosi_buf <<= 1;
         #(SPI_PERIOD/2) spi_SCLK = 0;
-        spi_MOSI = mosi_buf[WORD_WIDTH-1];
-        miso_buf[0] = spi_MISO;
     end
-    $display("sent: %x, recevied: %x", x, miso_buf);
+    assert (dataReady == '1) else $error("dataReady not asserted after complete transmission");
+    assert (rxShiftReg == x) else $error("Sent %x but %x got received.", x, rxShiftReg);
 end
 endtask : spi_xfer
 
