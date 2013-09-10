eeabdc10674e7ba0e712202be4a1d5f46367ad00
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2013-09-09 23:26:22 -0400

    [SPI] Test both directions of data transfer.

--- spi/spi_test.sv
+++ spi/spi_test.sv
@@ -24,30 +24,44 @@ spi_serdes #(.PACKET_WIDTH(PACKET_WIDTH)) u1
     (.clk, .spi_SCLK, .spi_SSEL, .spi_MOSI, .spi_MISO,
      .txData, .load, .rxShiftReg, .dataReady);
 
-logic [PACKET_WIDTH-1:0] dataReceived;
-always @(posedge clk) if (dataReady) dataReceived <= rxShiftReg;
+logic [PACKET_WIDTH-1:0] masterDataReceived, slaveDataReceived;
+always @(posedge clk) if (dataReady) slaveDataReceived <= rxShiftReg;
 
 // clock is active high and the first sampling happens on the first falling edge.
 
-task spi_xfer(logic [PACKET_WIDTH-1:0] x);
+task spi_xfer(logic [PACKET_WIDTH-1:0] masterToSlave, logic [PACKET_WIDTH-1:0] slaveToMaster);
 begin
     int bitIdx;
     
+    // Load data
+    load = '1;
+    txData = slaveToMaster;
+    @(posedge clk);
+    @(negedge clk);
+    load <= '0;
+    txData <= 'x;
+
     // Let's try to mimic how McSPI is configured: master data output begins a half-clock
     // before the first posedge.
     spi_SCLK = 0;
     #(SPI_PERIOD);
-    $display("spi_xfer %x", x);
-    dataReceived = 'x;
+    $display("spi_xfer %x", masterToSlave);
+    slaveDataReceived = 'x;
+    masterDataReceived = 'x;
     
     // now start ticking. Advance at negedge.
     for (bitIdx=PACKET_WIDTH-1; bitIdx>=0; bitIdx--) begin
         assert (dataReady == '0) else $error("Premature dataReady");
-        spi_MOSI = x[bitIdx];
+        spi_MOSI = masterToSlave[bitIdx];
         #(SPI_PERIOD/2) spi_SCLK = 1;
+        masterDataReceived[bitIdx] = spi_MISO;
         #(SPI_PERIOD/2) spi_SCLK = 0;
     end
-    assert (rxShiftReg == x) else $error("Sent %x but %x got received.", x, rxShiftReg);
+    assert (slaveDataReceived == masterToSlave)
+      else $error("Master sent %x but slave received %x.", masterToSlave, slaveDataReceived);
+    assert (masterDataReceived == slaveToMaster)
+      else $error("Master received %x but slave sent %x.", masterDataReceived, slaveToMaster);
+
 end
 endtask : spi_xfer
 
@@ -64,8 +78,8 @@ initial begin
     // Start SPI transmission
     spi_SSEL = 0;
     
-    spi_xfer(8'hab);
-    spi_xfer(8'h15);
+    spi_xfer(8'hab, 8'hff);
+    spi_xfer(8'h15, 8'ha5);
     
     spi_SSEL = 1;
     #(SPI_PERIOD);
