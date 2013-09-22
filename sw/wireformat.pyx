84a12c642dcc9c6284a5e7c88f32e2f7b703f809
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2013-09-21 23:57:20 -0400

    SPI wireformat cython: take dest array as parameter. Not used yet.

--- sw/wireformat.pyx
+++ sw/wireformat.pyx
@@ -1,9 +1,10 @@
 import numpy as np
 cimport numpy as np
 
-def spi_to_fixeds(np.uint8_t[:] spi):
-    cdef int n_words = len(spi) / 8
-    cdef np.ndarray[np.uint64_t, ndim=1] result = np.empty(n_words, dtype=np.uint64)
+def spi_to_fixeds(np.uint8_t[:] spi not None, np.uint64_t[:] result not None):
+    cdef int n_words = spi.shape[0] / 8
+    if result.shape[0] != n_words:
+        raise ValueError("Bad dimensionality for fixed-point result array (should be %d)", n_words)
     cdef int start = 0
     cdef np.uint64_t hiword, loword
     for i in range(n_words):
