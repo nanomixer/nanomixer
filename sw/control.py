30554ba4729e889fa7d414252654aedf7d191cca
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2013-09-28 21:52:48 -0400

    Correct name for the SPI device.

--- sw/control.py
+++ sw/control.py
@@ -243,7 +243,7 @@ def pack_meter_packet(rev, meter_data):
         rev=rev)
 
 
-spi_dev = spidev.SpiChannel('/dev/spi4.0', bits_per_word=20)
+spi_dev = spidev.SpiChannel('/dev/spidev4.0', bits_per_word=20)
 spi_channel = SPIChannel(spi_dev, buf_size_in_words=1024)
 io_thread = IOThread(param_mem_size=2048, spi_channel=spi_channel)
 io_thread.start()
