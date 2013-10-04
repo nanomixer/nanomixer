0878ff73fa7c5ef77c5b87290a6146f20710b680
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2013-10-04 00:34:47 -0400

    Broadcast updates to all clients.

--- sw/dummy_server.py
+++ sw/dummy_server.py
@@ -76,10 +76,18 @@ controller = DummyController()
 class Resource(BaseNamespace):
     def __init__(self, *a, **kw):
         super(Resource, self).__init__(*a, **kw)
-        self.response_state = dict(controller.full_state)
+        self.session['response_state'] = dict(controller.full_state)
 
     def disconnect(self, *a, **kw):
         super(Resource, self).disconnect(*a, **kw)
+
+    def set_all_response_states(self, control, value):
+        # This feels hacky, but it's how https://github.com/abourget/gevent-socketio/blob/master/socketio/mixins.py does it.
+        for sessid, socket in self.socket.server.sockets.iteritems():
+            response_state = socket.session.get('response_state', None)
+            if response_state is None:
+                continue
+            response_state[control] = value
 
     def on_msg(self, msg):
         try:
@@ -87,15 +95,17 @@ class Resource(BaseNamespace):
             for control, value in msg['state'].iteritems():
                 handled = controller.apply_update(control, value)
                 if handled:
-                    self.response_state[control] = value
+                    self.set_all_response_states(control, value)
                 else:
                     print "Oops, couldn't handle", control, value
 
+
+
             self.emit('msg', dict(
                 seq=seq,
-                state=self.response_state,
+                state=self.session['response_state'],
                 meter=(controller.meter_levels + np.sin(2*np.pi*time.time())).tolist()))
-            self.response_state = {}
+            self.session['response_state'] = {}
         except Exception as e:
             traceback.print_exc()
 
