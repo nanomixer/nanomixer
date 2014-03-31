5493ca7015d5c7ba1acab909dda8e924f32d76a4
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2014-03-30 23:30:38 -0400

    Refactor of state management, part 1: factor out get_initial_state.

--- sw/control.py
+++ sw/control.py
@@ -8,7 +8,6 @@ from dsp_program import (
     address_for_mixdown_gain,
     constants, meter_filter_param_base, StateVarFilter)
 import logging
-from collections import namedtuple
 import time
 import json
 from datetime import datetime
@@ -37,18 +36,15 @@ def pack_biquad_coeffs(b, a):
 PAN_LAW_DB = 3.
 panning_exponent = PAN_LAW_DB / (20*np.log10(2.))
 
-import re
-bus_name_re = re.compile(r'^b(?P<bus>\d+)/name$')
-fader_re = re.compile(r'^b(?P<bus>\d+)/c(?P<chan>\d+)/(lvl|pan)$')
-master_fader_re = re.compile(r'b(?P<bus>\d+)/(lvl|pan)')
-filter_re = re.compile(r'^c(?P<chan>\d+)/f(?P<filt>\d+)/(?P<param>type|freq|gain|q)$')
-bus_filter_re = re.compile(r'^b(?P<bus>\d+)/f(?P<filt>\d+)/(?P<param>type|freq|gain|q)$')
-channel_name_re = re.compile(r'c(?P<chan>\d+)/name$')
 
-Fader = namedtuple('Fader', 'level, pan')
-Filter = namedtuple('Filter', 'type, freq, gain, q')
-Channel = namedtuple('Channel', 'name, filters')
-Bus = namedtuple('Bus', 'downmix, filters')
+# Just using a class so that state_names.bus works.
+class state_names(object):
+    bus = 'b{bus}/{param}'
+    channel = 'c{channel}/{param}'
+    fader = 'b{bus}/c{channel}/{param}'
+    channel_filter = 'c{channel}/f{filt}/{param}'
+    bus_filter = 'b{bus}/f{filt}/{param}'
+
 
 metadata = dict(
     num_busses=HARDWARE_PARAMS['num_cores'] * HARDWARE_PARAMS['num_busses_per_core'] / 2, # HACK.
@@ -71,89 +67,66 @@ class InvalidSnapshot(Exception):
     pass
 
 
+
+def get_initial_state(metadata):
+    state = {}
+
+    def set_state_params(name_format, base_kv, **kw):
+        for k, v in kw.iteritems():
+            name = name_format.format(param=k, **base_kv)
+            state[name] = v
+
+    for bus in range(metadata['num_busses']):
+        if bus == 0:
+            name = "Master"
+        else:
+            name = "Aux {}".format(bus)
+        # Masters
+        set_state_params(state_names.bus, dict(bus=bus), name=name, lvl=0., pan=0.)
+
+        # Downmix
+        for channel in range(metadata['num_channels']):
+            set_state_params(state_names.fader, dict(bus=bus, channel=channel), lvl=MIN_FADER, pan=0.)
+
+        # Bus filters
+        for filt, freq in enumerate(initial_filter_frequencies):
+            assert metadata['num_biquads_per_bus'] == len(initial_filter_frequencies)
+            if filt == 0:
+                typ = 'lowshelf'
+            elif filt == len(initial_filter_frequencies) - 1:
+                typ = 'highshelf'
+            else:
+                typ = 'peaking'
+            set_state_params(state_names.bus_filter, dict(bus=bus, filt=filt),
+                type=typ, freq=freq, gain=0., q=np.sqrt(2.)/2)
+
+
+    for channel in range(metadata['num_channels']):
+        assert metadata['num_biquads_per_channel'] == len(initial_filter_frequencies)
+        set_state_params(state_names.channel, dict(channel=channel), name="Ch{}".format(channel+1))
+
+        for filt, freq in enumerate(initial_filter_frequencies):
+            if filt == 0:
+                typ = 'lowshelf'
+            elif filt == len(initial_filter_frequencies) - 1:
+                typ = 'highshelf'
+            else:
+                typ = 'peaking'
+            set_state_params(state_names.channel_filter, dict(channel=channel, filt=filt),
+                type=typ, freq=freq, gain=0., q=np.sqrt(2.)/2)
+
+    state['metadata'] = metadata
+
+    return state
+
+
 class BaseController(object):
     def __init__(self, snapshot_base_dir='snapshots'):
         self.snapshot_base_dir = snapshot_base_dir
         if not os.path.exists(self.snapshot_base_dir):
             os.makedirs(self.snapshot_base_dir)
 
-        self.routes = [
-            [bus_name_re, None],
-            [fader_re, self.update_for_fader],
-            [filter_re, self.update_for_channel_filter],
-            [bus_filter_re, self.update_for_bus_filter],
-            [master_fader_re, self.update_for_fader],
-            [channel_name_re, None]]
-
-        def set_initial_state(name, val):
-            self.state[name] = val
-
-        self.state = {}
-
-        self.busses = []
-        for bus in range(metadata['num_busses']):
-            chan_params = []
-            if bus == 0:
-                name = "Master"
-            else:
-                name = "Aux {}".format(bus)
-            # Masters
-            set_initial_state('b{bus}/name'.format(bus=bus), name)
-            set_initial_state('b{bus}/lvl'.format(bus=bus), 0.)
-            set_initial_state('b{bus}/pan'.format(bus=bus), 0.)
-
-            # Downmix
-            for channel in range(metadata['num_channels']):
-                level_name = 'b{bus}/c{chan}/lvl'.format(bus=bus, chan=channel)
-                pan_name = 'b{bus}/c{chan}/pan'.format(bus=bus, chan=channel)
-                chan_params.append(Fader(level_name, pan_name))
-                set_initial_state(level_name, MIN_FADER)
-                set_initial_state(pan_name, 0.)
-
-            # Bus filters
-            bus_filters = []
-            for filt, freq in enumerate(initial_filter_frequencies):
-                names = {
-                    param: 'b{bus}/f{filt}/{param}'.format(bus=bus, filt=filt, param=param)
-                    for param in ['type', 'freq', 'gain', 'q']}
-                bus_filters.append(Filter(**names))
-                if filt == 0:
-                    typ = 'lowshelf'
-                elif filt == len(initial_filter_frequencies) - 1:
-                    typ = 'highshelf'
-                else:
-                    typ = 'peaking'
-                set_initial_state(names['type'], typ)
-                set_initial_state(names['freq'], freq)
-                set_initial_state(names['gain'], 0.)
-                set_initial_state(names['q'], np.sqrt(2.)/2)
-            self.busses.append(Bus(downmix=chan_params, filters=bus_filters))
-
-
-        self.channels = []
-        for channel in range(metadata['num_channels']):
-            assert metadata['num_biquads_per_channel'] == 5
-            filts = []
-            chan_name = "c{chan}/name".format(chan=channel)
-            self.channels.append(Channel(chan_name, filts))
-            set_initial_state(chan_name, "Ch{}".format(channel+1))
-            for filt, freq in enumerate(initial_filter_frequencies):
-                names = {
-                    param: 'c{chan}/f{filt}/{param}'.format(chan=channel, filt=filt, param=param)
-                    for param in ['type', 'freq', 'gain', 'q']}
-                filts.append(Filter(**names))
-                if filt == 0:
-                    typ = 'lowshelf'
-                elif filt == len(initial_filter_frequencies) - 1:
-                    typ = 'highshelf'
-                else:
-                    typ = 'peaking'
-                set_initial_state(names['type'], typ)
-                set_initial_state(names['freq'], freq)
-                set_initial_state(names['gain'], 0.)
-                set_initial_state(names['q'], np.sqrt(2.)/2)
-
-        self.state['metadata'] = metadata
+        self.state = get_initial_state()
 
         try:
             self.load_snapshot()
