5fad4ff2a087d615312aa7474c164479769c99b9
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2014-04-04 18:32:39 -0400

    Edit the right file.

--- fpga_v2/adat_in.sv
+++ fpga_v2/adat_in.sv
@@ -17,13 +17,14 @@ bit [5:0] sync_counter;
 bit [2:0] tic_counter;
 bit [7:0] current_bit;
 bit [0:245] adat_deser;
+bit transition_happened;
 
 // State-independent sequential logic:
 always_ff @(posedge clk) begin
 	adat_s1 <= adat_async;
 	adat_s2 <= adat_s1;
-	adat_synced <= adat_s2; // Synchronize raw ADAT bitstream 
-	
+	adat_synced <= adat_s2; // Synchronize raw ADAT bitstream
+
 	if (adat_synced != adat_s2)
 		sync_counter <= '0; // Reset sync-detect counter whenever input changes
 	else
@@ -45,19 +46,25 @@ always_ff @(posedge clk) begin
 	end
 	else state <= next; // Increment state if not in reset
 
-	tic_counter = tic_counter + 2'b1; // Increment tic counter
-	
+	transition_happened = adat_synced != adat_s2;
+
+	if (transition_happened) begin
+		tic_counter = 0;
+	end else begin
+		tic_counter = tic_counter + 2'b1; // Increment tic counter
+	end
+
 	unique case (state)
 		SYNC_WAIT: last_sample <= adat_synced; // Set "last sample" to current value
-	
+
 		SYNC: begin
 			tic_counter = 0; // Reset tic counter
 			current_bit <= 0; // Reset bit counter
 			data_ready = 0; // Clear data ready strobe
 		end
-		
+
 		BIT_WAIT: begin
-		
+
 		end
 
 		BIT_SAMPLE: begin
@@ -65,15 +72,15 @@ always_ff @(posedge clk) begin
 			last_sample <= adat_synced; // Save sample value
 			current_bit <= current_bit + 2'b1; // Increment bit counter
 		end
-			
+
 		PARSE: begin
 			data_valid = 1;
 			for (int i = 0; i <= 245; i = i + 5) begin
 				data_valid = data_valid & adat_deser[i]; // Make sure all "1" bits are actually "1"
 			end
-			
+
 			{timecode, midi, smux} = adat_deser[1:3]; // Collect ADAT user bits 0-2 (3 is reserved)
-			
+
 			for (int c = 0; c <= 7; c++) begin // Loop through all channels and collect data
 				audio_bus[c] = { adat_deser[(6 + 30*c + 0) +: 4],
 									  adat_deser[(6 + 30*c + 5) +: 4],
@@ -82,18 +89,18 @@ always_ff @(posedge clk) begin
 									  adat_deser[(6 + 30*c + 20) +: 4],
 									  adat_deser[(6 + 30*c + 25) +: 4] };
 			end
-			
+
 			data_ready = 1; // Set data ready strobe
 		end
 	endcase
-end 
+end
 
 // State machine logic (combinatorial)
 always_comb begin
 	next = state;
 	unique case (state)
 		IDLE			: if (sync_counter > 60)		next = SYNC_WAIT; // Detect sync pattern
-		SYNC_WAIT	: if (adat_synced != adat_s2)	next = SYNC; // Detect first transition
+		SYNC_WAIT	: if (transition_happened)	next = SYNC; // Detect first transition
 		SYNC			: 										next = BIT_WAIT;
 		BIT_WAIT		: if (tic_counter == 3)			next = BIT_SAMPLE; // Samples center of bits
 		BIT_SAMPLE	: if (current_bit == 245)	next = PARSE;
@@ -102,4 +109,4 @@ always_comb begin
 	endcase
 end
 
-endmodule 
+endmodule
